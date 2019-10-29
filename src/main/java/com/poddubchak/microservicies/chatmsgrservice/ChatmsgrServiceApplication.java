272ad1d9408d20ce7f6b06434a57ef5054925a8c
VladimirPoddubchak <45694858+VladimirPoddubchak@users.noreply.github.com>
2019-10-29 13:13:44 +1000

    init commit

---
+++ src/main/java/com/poddubchak/microservicies/chatmsgrservice/ChatmsgrServiceApplication.java
@@ -0,0 +1,13 @@
+package com.poddubchak.microservicies.chatmsgrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatmsgrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatmsgrServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poddubchak/microservicies/chatmsgrservice/ChatmsgrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poddubchak.microservicies.chatmsgrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatmsgrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
